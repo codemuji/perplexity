a2406dceaf025c7ef649710b610420cf452367c8
codemuji <rajatpradhan011@gmail.com>
2026-03-12 09:21:29 +0530

    feat: implement user registration with validation and email notification

---
+++ src/services/mail.service.js
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.GOOGLE_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+transporter
+  .verify()
+  .then(() => {
+    console.log("Email transporter is ready to send emails");
+  })
+  .catch((err) => {
+    console.error("Email transporter verification failed:", err);
+  });
+
+export async function sendEmail({ to, subject, html, text }) {
+  const mailOptions = {
+    from: process.env.GOOGLE_USER,
+    to,
+    subject,
+    html,
+    text,
+  };
+
+  const details = await transporter.sendMail(mailOptions);
+  console.log("Email sent:", details);
+}
+
+
