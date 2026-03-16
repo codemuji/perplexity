649572774b4fb80574e38a55cc0243f4630d8567
codemuji <rajatpradhan011@gmail.com>
2026-03-16 22:19:30 +0530

    feat: implement authentication hooks for registration, login, and user data retrieval

--- Frontend/src/features/auth/hook/useAuth.js
+++ Frontend/src/features/auth/hook/useAuth.js
@@ -1,3 +1,46 @@
 import { useDispatch } from "react-redux";
 import { register, login, getMe } from "../service/auth.api.js";
+import { setUser, setLoading, setError } from "../auth.slice.js";
 
+export function useAuth() {
+  const dispatch = useDispatch();
+
+  async function handleRegister(email, username, passoword) {
+    try {
+      dispatch(setLoading(true));
+      const data = await register({ email, username, passoword });
+    } catch (error) {
+      dispatch(
+        setError(error.response?.data?.message || "Registration failed"),
+      );
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  async function handleLogin(email, passoword) {
+    try {
+      dispatch(setLoading(true));
+      const data = await login({ email, passoword });
+      dispatch(setUser(data.user));
+    } catch (err) {
+      dispatch(setError(err.response?.data?.message || "Login failed"));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  async function handleGetMe() {
+    try {
+      dispatch(setLoading(true));
+      const data = await getMe();
+      dispatch(setUser(data.user));
+    } catch (err) {
+      dispatch(
+        setError(err.response?.data?.message || "Failed to fetch user data"),
+      );
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+}
