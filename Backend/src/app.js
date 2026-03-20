dfa1eaa25c502bc48a646b7e9434db49eaa32d25
codemuji <rajatpradhan011@gmail.com>
2026-03-20 08:51:42 +0530

    feat: add chat functionality with message handling, chat title generation, and routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,18 +3,21 @@ import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
 import morgan from "morgan";
-
+import chatRouter from "./routes/chat.routes.js";
 const app = express();
 //midlleware
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+  }),
+);
 
 app.use("/api/auth", authRouter);
+app.use("/api/chats", chatRouter);
 export default app;
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,8 +1,18 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import {
+  HumanMessage,
+  SystemMessage,
+  AIMessage,
+} from "@langchain/core/messages";
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY,
+});
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
 });
 
 // export async function testAi() {
@@ -10,3 +20,29 @@ const model = new ChatGoogleGenerativeAI({
 //     console.log(response.text);
 //   });
 // }
+
+export async function generateResponse(messages) {
+  const response = await model.invoke(
+    messages.map((msg) => {
+      if (msg.role === "user") {
+        return new HumanMessage(msg.content);
+      } else if (msg.role === "ai") {
+        return new AIMessage(msg.content);
+      }
+    }),
+  );
+  return response.text;
+}
+
+export async function generateChatTitle(message) {
+  const response = await mistralModel.invoke([
+    new SystemMessage(
+      "You are a helpful assistant that generates concise and relevant chat titles based on the content of the conversation. The title should be no more than 5 words and should capture the essence of the discussion. Please provide a title that accurately reflects the main topic or theme of the conversation.",
+    ),
+    new HumanMessage(
+      `Generate a chat title for the following conversation: ${message}`,
+    ),
+  ]);
+
+  return response.text;
+}
